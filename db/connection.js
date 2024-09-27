933b599d1afd1702c4a4891fd1cbdccacd956b92
ivanaplese <ivanaplese123@gmail.com>
2024-09-27 15:38:26 +0200

    spasavanje kolegija part 1: ispravljen mongo

--- db/connection.js
+++ db/connection.js
@@ -1,54 +1,25 @@
-
 import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
 
-const connectionString = "mongodb+srv://test:test@cluster0.vinwyba.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+dotenv.config();
 
-const client = new MongoClient(connectionString, {
-    tlsAllowInvalidCertificates: true, // Dodano za ignoriranje nevaljanih SSL certifikata
-});
+const url = process.env.MONGO_URI; // Use the correct environment variable name
+const client = new MongoClient(url); // No need for useNewUrlParser or useUnifiedTopology
 
+let db;
 
-let db = null;
-
-try {
-    console.log("Trying to establish connection...");
-    // Pokušaj konekciju s MongoDB serverom
-    const conn = await client.connect();
-    console.log("Connected to MongoDB!");
-
-    // Postavi bazu podataka na 'cnr'
-    db = conn.db("cnr");
-} catch (e) {
-    console.error("Error establishing connection:", e);
+async function connectDB() {
+    if (!db) {
+        try {
+            await client.connect();
+            db = client.db(process.env.DB_NAME); // Ensure DB_NAME is set correctly in your .env file
+            console.log("Connected to MongoDB");
+        } catch (error) {
+            console.error("Failed to connect to MongoDB", error);
+            throw error;
+        }
+    }
+    return db;
 }
 
-// Export baze podataka (ako nije uspješna konekcija, db će biti null)
-export default db;
-
-
-
-// import { MongoClient } from "mongodb";
-
-// const connectionString = "mongodb+srv://test:test@cluster0.vinwyba.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-// let db = null;
-
-// const connectToDatabase = async () => {
-//     if (db) {
-//         return db;
-//     }
-//     try {
-//         const client = new MongoClient(connectionString, {
-//             tlsAllowInvalidCertificates: true,
-//         });
-//         const connection = await client.connect();
-//         db = connection.db("cnr");
-//         console.log("Connected to MongoDB!");
-//         return db;
-//     } catch (e) {
-//         console.error("Error connecting to MongoDB:", e);
-//         throw e;
-//     }
-// };
-
-// export default connectToDatabase;
+export default connectDB;
