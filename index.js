e859a4b3b778b033a1c91391b935df2ecd4f1186
ivanaplese <ivanaplese123@gmail.com>
2024-09-29 21:43:11 +0200

    m o l i m

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 8080;
 
 // CORS middleware
 app.use(cors({
-    origin: ['https://cars-n-rides.netlify.app/', 'http://localhost:8081', 'http://localhost:8080'],
+    origin: ['http://localhost:8080', 'http://localhost:8081', 'cars-n-rides.netlify.app'],
     methods: ['GET,HEAD,PUT,PATCH,POST,DELETE'],
     credentials: true
 }));
